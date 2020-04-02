bf44cd693658d6167c5d4a4dd6b139725e20e460
BrendaNaggayi <61741057+BrendaNaggayi@users.noreply.github.com>
2020-04-02 08:17:40 +0300

    Add files via upload
    added files

---
+++ home.js
@@ -0,0 +1,7 @@
+var movieList = document.getElementsByClassName('movie-list');
+var searchBar = document.getElementsByClassName('search-txt');
+
+searchBar.addEventListener('keyUp',(e) => {
+console.log(e);
+}
+)
